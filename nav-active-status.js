c52a5f3decba058b5529e867a60a993c79cd5a10
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-15 09:25:17 +0600

    add normal font black color

--- nav-active-status.js
+++ nav-active-status.js
@@ -1,12 +1,7 @@
 const ulTag = document.getElementById("nav-container");
-
 const listItems = ulTag.getElementsByTagName("li")
-
 let lastClicked = null;
-
-
 for (let singleListItem of listItems) {
-
     singleListItem.addEventListener("click", function () {
         console.log(lastClicked);
         if (lastClicked == null) {
@@ -14,19 +9,12 @@ for (let singleListItem of listItems) {
             singleListItem.style.backgroundColor = ("red")
             singleListItem.style.color = ("whit")
             lastClicked = singleListItem
-
         } else {
-
             lastClicked.style.backgroundColor = "transparent"
-            lastClicked.style.color = "#eee"
-
-
+            lastClicked.style.color = "#000000"
             singleListItem.style.backgroundColor = "red"
             singleListItem.style.color = "whit"
             lastClicked = singleListItem
         }
-
-
-
     })
 }
