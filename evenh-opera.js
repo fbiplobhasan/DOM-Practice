5a4cf29eeb65bd6d41c92a25f34c523fd84df0d6
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-15 22:50:25 +0600

    add event handler operation

---
+++ evenh-opera.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById('btn-post').addEventListener('click', function () {
+
+    // 2nd step
+    const commentBox = document.getElementById('new-comment');
+    const newComment = commentBox.value;
+    console.log(newComment);
+
+    // 3rd step
+
+    const commentContainer = document.getElementById('comment-container')
+    const p = document.createElement('p')
+    p.innerText = newComment;
+    commentContainer.appendChild(p);
+
+    // 4rth step
+
+    commentBox.value = '';
+})
